62553f8ae07046f49de14b51cdc001224fcae3e3
Shane Powell <shane.powell@ipfx.com>
2023-03-07 07:30:24 +1300

    Fix compiler breaks in the DotNetStandard feature branch
    Update nuget packages
    Add missing nuget references: Microsoft.Extensions.Http and Microsoft.Extensions.Options.ConfigurationExtensions
    Change HttpClient.Send call to HttpClient.SendAsync fixing the code broken due to this change.

--- OpenAI.GPT3.DotNetStandard/Extensions/HttpclientExtensions.cs
+++ OpenAI.GPT3.DotNetStandard/Extensions/HttpclientExtensions.cs
@@ -25,7 +25,7 @@ namespace OpenAI.GPT3.DotNetStandard.Extensions
             return await response.Content.ReadFromJsonAsync<TResponse>() ?? throw new InvalidOperationException();
         }
 
-        public static HttpResponseMessage PostAsStreamAsync(this HttpClient client, string uri, object requestModel)
+        public static async Task<HttpResponseMessage> PostAsStreamAsync(this HttpClient client, string uri, object requestModel)
         {
             var settings = new JsonSerializerOptions()
             {
@@ -38,7 +38,7 @@ namespace OpenAI.GPT3.DotNetStandard.Extensions
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/event-stream"));
             request.Content = content;
 
-            return client.Send(request, HttpCompletionOption.ResponseHeadersRead);
+            return await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
         }
 
         public static async Task<TResponse> PostFileAndReadAsAsync<TResponse>(this HttpClient client, string uri, HttpContent content)
