4cc44f773371301e7ac8599427dd66b71cb8a96f
Shane Powell <shane.powell@ipfx.com>
2023-11-22 07:29:00 +1300

    Change the Error code layout to be more inline with the current codebase and be backward compatible with the current version but still support openai error codes returned.

--- OpenAI.SDK/ObjectModels/ResponseModels/BaseResponse.cs
+++ OpenAI.SDK/ObjectModels/ResponseModels/BaseResponse.cs
@@ -51,7 +51,9 @@ public record DataBaseResponse<T> : BaseResponse
 
 public class Error
 {
-    [JsonPropertyName("code")] public object? Code { get; set; } // in openai this is a string, in open router this is a number!
+    [JsonPropertyName("code")] public object? CodeObject { get; set; } // in openai this is a string, in open router this is a number!
+    [JsonIgnore] public string? Code => CodeObject as string;
+    [JsonIgnore] public int? CodeAsInt => CodeObject as int?;
 
     [JsonPropertyName("param")] public string? Param { get; set; }
 
