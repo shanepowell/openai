584284b4cb6ce28d30057158232f8765bc36d186
Shane Powell <shane.powell@ipfx.com>
2024-03-26 13:09:42 +1300

    Small fixup and bump version.

--- OpenAI.SDK/ObjectModels/ResponseModels/GroqUsageResponse.cs
+++ OpenAI.SDK/ObjectModels/ResponseModels/GroqUsageResponse.cs
@@ -5,5 +5,5 @@ namespace OpenAI.ObjectModels.ResponseModels;
 public record GroqUsageResponse
 {
     [JsonPropertyName("id")] public string Id { get; set; }
-    [JsonPropertyName("usage")] public UsageResponse Usage { get; set; }
+    [JsonPropertyName("usage")] public UsageResponse? Usage { get; set; }
 }
