ff1a633e294c4ed56fe48719754bccde086932f2
Shane Powell <shane.powell@ipfx.com>
2025-07-31 10:07:40 +1200

    Add test to test the edit image requiring the mime type to work.

--- OpenAI.Playground/TestHelpers/ImageTestHelper.cs
+++ OpenAI.Playground/TestHelpers/ImageTestHelper.cs
@@ -158,6 +158,65 @@ internal static class ImageTestHelper
         }
     }
 
+    public static async Task RunSimpleCreateImageEditTest3(IOpenAIService sdk)
+    {
+        ConsoleExtensions.WriteLine("Image Edit Create Testing is starting:", ConsoleColor.Cyan);
+        const string logoFileName = "logo.png";
+
+        // Images should be in png format with ARGB. I got help from this website to generate sample mask
+        // https://www.online-image-editor.com/
+        var logoFile = await FileExtensions.ReadAllBytesAsync($"SampleData/{logoFileName}");
+
+        try
+        {
+            ConsoleExtensions.WriteLine("Image  Edit Create Test:", ConsoleColor.DarkCyan);
+            var imageResult = await sdk.Image.CreateImageEdit(new()
+            {
+                Model = "gpt-image-1",
+                Images = [new ImageEditFile{ Name = "logo", Content = logoFile, MimeType = "image/png"}],
+                Prompt = "A man with the logo (IPFX) (IP part is white) on it's black t-shirt",
+                N = 1,
+                Size = StaticValues.ImageStatics.Size.Size1024,
+                User = "TestUser"
+            });
+
+
+            if (imageResult.Successful)
+            {
+                var index = 0;
+                foreach (var image in imageResult.Results)
+                {
+                    var filePath = Path.Combine(Path.GetTempPath(), $"TempImage{index}.png");
+                    index++;
+
+                    ConsoleExtensions.WriteLine($"Image: {filePath}", ConsoleColor.DarkCyan);
+
+                    File.WriteAllBytes(filePath, Convert.FromBase64String(image.B64));
+
+                    var psi = new ProcessStartInfo(filePath)
+                    {
+                        UseShellExecute = true
+                    };
+                    Process.Start(psi);
+                }
+            }
+            else
+            {
+                if (imageResult.Error == null)
+                {
+                    throw new("Unknown Error");
+                }
+
+                Console.WriteLine($"{imageResult.Error.Code}: {imageResult.Error.Message}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
     public static async Task RunSimpleCreateImagesEditTest(IOpenAIService sdk)
     {
         ConsoleExtensions.WriteLine("Image Edit Create Testing is starting:", ConsoleColor.Cyan);
